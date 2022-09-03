344021858dbd98ca9d816b03ab3b9de42103c357
igor-paiva <igorbatistapaiva@outlook.com>
2022-09-03 20:16:20 -0300

    Configure frontend

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,77 +1,60 @@
-
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
-  root: true,
-
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
-  },
-
-  env: {
-    browser: true,
-    'vue/setup-compiler-macros': true
-  },
-
-  // Rules order is important, please avoid shuffling them
-  extends: [
-    // Base ESLint recommended rules
-    // 'eslint:recommended',
-
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules (look for Vuejs 2 ones)
-    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
-    'airbnb-base'
-    
-  ],
-
-  plugins: [
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-    // required to lint *.vue files
-    'vue',
-
-    
-  ],
-
-  globals: {
-    ga: 'readonly', // Google Analytics
-    cordova: 'readonly',
-    __statics: 'readonly',
-    process: 'readonly',
-    Capacitor: 'readonly',
-    chrome: 'readonly'
-  },
-
-  // add your custom rules here
-  rules: {
-    
-    'no-param-reassign': 'off',
-    'no-void': 'off',
-    'no-nested-ternary': 'off',
-    'max-classes-per-file': 'off',
-
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
-    
-    'prefer-promise-reject-errors': 'off',
-
-    'vue/multi-word-component-names': 'off',
-
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    'env': {
+        'browser': true,
+        'vue/setup-compiler-macros': true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'airbnb-base',
+        'plugin:prettier/recommended'
+    ],
+    'globals': {
+        'Capacitor': 'readonly',
+        '__statics': 'readonly',
+        'chrome': 'readonly',
+        'cordova': 'readonly',
+        'ga': 'readonly',
+        'process': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'parser': '@babel/eslint-parser',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'vue'
+    ],
+    'root': true,
+    'rules': {
+        'func-names': 'off',
+        'import/default': 'error',
+        'import/export': 'error',
+        'import/extensions': 'off',
+        'import/first': 'off',
+        'import/named': 'error',
+        'import/namespace': 'error',
+        'import/no-extraneous-dependencies': 'off',
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'max-classes-per-file': 'off',
+        'no-debugger': 'off',
+        'no-nested-ternary': 'off',
+        'no-param-reassign': 'off',
+        'no-void': 'off',
+        'prefer-promise-reject-errors': 'off',
+        'eol-last': ['error', 'always'],
+        'prettier/prettier': [
+            'error',
+            {
+                'arrowParens': 'always',
+                'bracketSpacing': true,
+                'semi': true,
+                'singleQuote': true,
+                'tabWidth': 2,
+                'trailingComma': 'all'
+            }
+        ],
+        'no-plusplus': 'off',
+        'vue/multi-word-component-names': 'off'
+    }
+};
--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -22,15 +22,10 @@ module.exports = function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v1.quasar.dev/quasar-cli/boot-files
-    boot: [
-
-      'axios',
-    ],
+    boot: [],
 
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
-    css: [
-      'app.scss',
-    ],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -48,7 +43,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
 
@@ -69,7 +64,8 @@ module.exports = function (/* ctx */) {
       // https://v1.quasar.dev/quasar-cli/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
       chainWebpack(chain) {
-        chain.plugin('eslint-webpack-plugin')
+        chain
+          .plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]);
       },
     },
@@ -100,7 +96,7 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Dialog'],
     },
 
     // animations: 'all', // --- includes all animations
@@ -170,13 +166,11 @@ module.exports = function (/* ctx */) {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
